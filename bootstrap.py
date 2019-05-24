427566fd23208a26e00bead01fc4e3cebdbd454d
Jebaitedneko <kannanpulimoottil@gmail.com>
2019-05-25 04:08:51 +0530

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -530,6 +530,8 @@ class GitCloneMusicbot(SetupTask):
 
         import pip
         pip.main("install --upgrade -r requirements.txt".split())
+        import pur
+        pur.main("--only youtube_dl".split())
 
 
 class SetupMusicbot(SetupTask):
